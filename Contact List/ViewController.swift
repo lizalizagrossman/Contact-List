d616e3473f4d996e9d892f7fb78f9bcdbe91674f
Liza Grossman <lisok.sam@gmail.com>
2023-03-13 19:16:17 +0200

    Initial Commit

---
+++ Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact List
+//
+//  Created by Elizabeth on 13/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
